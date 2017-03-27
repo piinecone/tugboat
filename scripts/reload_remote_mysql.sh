1401faf0e5eb135ef668b49e3ab9397e77ca9f41
Nicholas Hughes <hughes.nick@gmail.com>
2017-03-27 17:04:13 +0100

    Drop target database during remote reload

--- scripts/reload_remote_mysql.sh
+++ scripts/reload_remote_mysql.sh
@@ -6,4 +6,5 @@ DUMPFILE=$3
 DBNAME=$4
 
 kubectl exec -i $POD -- bash -c "cat > ${DBNAME}.sql" < $DUMPFILE
+kubectl exec -i $POD -- bash -c "mysql -u root -p${PASSWORD} -e 'DROP DATABASE ${DBNAME}; CREATE DATABASE ${DBNAME};'"
 kubectl exec -i $POD -- bash -c "mysql -u root -p${PASSWORD} ${DBNAME} < ${DBNAME}.sql"
